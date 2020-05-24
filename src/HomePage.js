2964004f9ab335940e68beb5dc3428c8922920ea
w0utje36 <34859081+w0utje36@users.noreply.github.com>
2020-05-24 15:25:58 +0200

    Add files via upload

---
+++ src/HomePage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    return <div>Home page</div>;
+  }
+}
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Counters from './Counters';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Counters />
   </React.StrictMode>,
   document.getElementById('root')
 );
